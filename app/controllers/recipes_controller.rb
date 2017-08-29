76798622c89161cce7fda940263284b5781b0e31
danizaghian <dani.zaghian@gmail.com>
2017-08-28 20:26:46 -0700

    added route for all recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,11 @@
 class RecipesController < ApplicationController
 	def single_recipe_method
-		@recipe = Recipe.first
+		@recipe = Recipe.first #single recipe hash
 		render "single_recipe.html.erb"
+	end
+
+	def all_recipes_method
+		@recipes = Recipe.all #array of recipe hashes
+		render "all_recipes.html.erb"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/single_recipe" => "recipes#single_recipe_method"
+  get "/all_recipes" => "recipes#all_recipes_method"
 end
