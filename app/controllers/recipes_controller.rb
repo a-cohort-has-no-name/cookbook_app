ad1cfdba248ea3f61a9fcf6ae331fd6889419d38
danizaghian <dani.zaghian@gmail.com>
2017-08-30 21:13:31 -0700

    added a new recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,21 @@ class RecipesController < ApplicationController
 		@recipes = Recipe.all #array of recipe hashes
 		render "all_recipes.html.erb"
 	end
+
+	def new_recipe_method
+		render "new_recipe.html.erb"
+	end
+
+	def new_recipe_result_method
+		@new_recipe = Recipe.new(
+			title: params[:title],
+			chef: params[:chef],
+			ingredients: params[:ingredients],
+			directions: params[:directions],
+			image: params[:image],
+			prep_time: params[:prep_time],
+			)
+		@new_recipe.save
+		render "new_recipe_result.html.erb"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/single_recipe" => "recipes#single_recipe_method"
   get "/all_recipes" => "recipes#all_recipes_method"
+
+  get "/new_recipe" => "recipes#new_recipe_method"
+  post "/new_recipe_result" => "recipes#new_recipe_result_method"
 end
