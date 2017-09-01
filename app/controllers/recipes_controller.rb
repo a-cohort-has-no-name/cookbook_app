91479cc8bd89d54cf86431c9c0f56addd3c2ccf6
danizaghian <dani.zaghian@gmail.com>
2017-08-31 21:16:29 -0700

    added edit and update actions with restful conventions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,14 +29,36 @@ class RecipesController < ApplicationController
 	end
 
 	def edit
-		
+		@recipe = Recipe.find_by(id: params[:id])
+		render "edit.html.erb"
 	end
 
 	def update
-		
+		@recipe = Recipe.find_by(id: params[:id]) 
+		@recipe.update(
+			title: params[:title],
+			chef: params[:chef],
+			ingredients: params[:ingredients],
+			directions: params[:directions],
+			image: params[:image],
+			prep_time: params[:prep_time],
+			)
+		render "update.html.erb"
 	end
 
 	def destroy
 		
 	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
