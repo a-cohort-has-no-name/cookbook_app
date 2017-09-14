fbbbb5e83babb9a0c51deccc9de9552a875be80c
danizaghian <dani.zaghian@gmail.com>
2017-09-13 19:46:26 -0700

    added authentication to users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      #sign user in, proceed normally
+      session[:user_id] = user.id
+      flash[:success] = "Successfully created account!"
+      redirect_to "/recipes"
+    else
+      #flash a warning and redirect to signup page
+      flash[:warning] = "Invalid email or password"
+      redirect_to "/signup"
+    end
+  end
+end
